de27474edd1bca194015cb1b1cac4b0f16c1d038
Emard <vordah@gmail.com>
2018-04-30 14:28:38 +0200

    ulx3s: all vendor specific modules instantiated from toplevel

--- proj/lattice/ulx3s-45k-usbjoy/top/amiga_ulx3s.vhd
+++ proj/lattice/ulx3s-45k-usbjoy/top/amiga_ulx3s.vhd
@@ -507,28 +507,17 @@ begin
   end if;
 end process;
 
-  -- this module instantiates vendor specific modules ddr_out to
+  -- vendor specific DDR modules
   -- convert SDR 2-bit input to DDR clocked 1-bit output (single-ended)
-  G_vgatext_ddrout_flea: entity work.ddr_dvid_out_se
-  port map
-  (
-    clk       => clk_pixel_shift,
-    clk_n     => clkn_pixel_shift,
-    in_red    => dvid_red,
-    in_green  => dvid_green,
-    in_blue   => dvid_blue,
-    in_clock  => dvid_clock,
-    out_red   => ddr_d(2),
-    out_green => ddr_d(1),
-    out_blue  => ddr_d(0),
-    out_clock => ddr_clk
-  );
-
-  gpdi_data_channels_flea: for i in 0 to 2 generate
-    gpdi_diff_data: OLVDS
+  flea_ddr_red:   ODDRX1F port map (D0=>dvid_red(0),   D1=>dvid_red(1),   Q=>ddr_d(2), SCLK=>clk_pixel_shift, RST=>'0');
+  flea_ddr_green: ODDRX1F port map (D0=>dvid_green(0), D1=>dvid_green(1), Q=>ddr_d(1), SCLK=>clk_pixel_shift, RST=>'0');
+  flea_ddr_blue:  ODDRX1F port map (D0=>dvid_blue(0),  D1=>dvid_blue(1),  Q=>ddr_d(0), SCLK=>clk_pixel_shift, RST=>'0');
+  flea_ddr_clock: ODDRX1F port map (D0=>dvid_clock(0), D1=>dvid_clock(1), Q=>ddr_clk,  SCLK=>clk_pixel_shift, RST=>'0');
+  flea_gpdi_data_channels: for i in 0 to 2 generate
+    flea_gpdi_diff_data: OLVDS
     port map(A => ddr_d(i), Z => gpdi_dp(i), ZN => gpdi_dn(i));
   end generate;
-  gpdi_diff_clock_flea: OLVDS
+  flea_gpdi_diff_clock: OLVDS
   port map(A => ddr_clk, Z => gpdi_clkp, ZN => gpdi_clkn);
 
 --gpdi_dp(2) <= LVDS_Red(0);
@@ -586,29 +575,17 @@ emard_video: if not C_flea_av generate
       out_clock => dvid_clock
   );
 
-  -- this module instantiates vendor specific modules ddr_out to
+  -- vendor specific DDR modules
   -- convert SDR 2-bit input to DDR clocked 1-bit output (single-ended)
-  G_vgatext_ddrout: entity work.ddr_dvid_out_se
-  port map
-  (
-    clk       => clk_pixel_shift,
-    clk_n     => clkn_pixel_shift,
-    in_red    => dvid_red,
-    in_green  => dvid_green,
-    in_blue   => dvid_blue,
-    in_clock  => dvid_clock,
-    out_red   => ddr_d(2),
-    out_green => ddr_d(1),
-    out_blue  => ddr_d(0),
-    out_clock => ddr_clk
-  );
-
+  ddr_red:   ODDRX1F port map (D0=>dvid_red(0),   D1=>dvid_red(1),   Q=>ddr_d(2), SCLK=>clk_pixel_shift, RST=>'0');
+  ddr_green: ODDRX1F port map (D0=>dvid_green(0), D1=>dvid_green(1), Q=>ddr_d(1), SCLK=>clk_pixel_shift, RST=>'0');
+  ddr_blue:  ODDRX1F port map (D0=>dvid_blue(0),  D1=>dvid_blue(1),  Q=>ddr_d(0), SCLK=>clk_pixel_shift, RST=>'0');
+  ddr_clock: ODDRX1F port map (D0=>dvid_clock(0), D1=>dvid_clock(1), Q=>ddr_clk,  SCLK=>clk_pixel_shift, RST=>'0');
+  -- vendor specific modules for differential output
   gpdi_data_channels: for i in 0 to 2 generate
-    gpdi_diff_data: OLVDS
-    port map(A => ddr_d(i), Z => gpdi_dp(i), ZN => gpdi_dn(i));
+    gpdi_diff_data: OLVDS port map (A => ddr_d(i), Z => gpdi_dp(i), ZN => gpdi_dn(i));
   end generate;
-  gpdi_diff_clock: OLVDS
-  port map(A => ddr_clk, Z => gpdi_clkp, ZN => gpdi_clkn);
+  gpdi_diff_clock: OLVDS port map (A => ddr_clk, Z => gpdi_clkp, ZN => gpdi_clkn);
 
 end generate;
 
