8dec54871d64626b8682c866bb7f98cf3c36cdcf
emard <vordah@gmail.com>
2019-11-17 23:50:36 +0100

    FFM-LFE5 support, it boots from SD card to floppy disk screen but currently PS/2 keyboard doesn't work for me.

---
+++ proj/lattice/ffm-lfe5-lcdif/universal_make_ps2kbd/files.mk
@@ -0,0 +1,76 @@
+VHDL_FILES = $(TOP_MODULE_FILE) \
+../../../../rtl_emard/lattice/ulx3s/clocks/$(CLK0_NAME)_vhdl.vhd \
+../../../../rtl_emard/lattice/ulx3s/clocks/$(CLK1_NAME)_vhdl.vhd \
+../../../../rtl_emard/lattice/ulx3s/clocks/$(CLK2_NAME)_vhdl.vhd \
+../../../../rtl_emard/lattice/ulx3s/clocks/$(CLK3_NAME)_vhdl.vhd \
+../../../../Minimig_ECS/impl1/source/Fampiga.vhd \
+../../../../source_emard/cfide.vhd \
+../../../../Minimig_ECS/impl1/source/Debounce.vhd \
+../../../../Minimig_ECS/impl1/source/TG68K.vhd \
+../../../../Minimig_ECS/impl1/source/TG68K_ALU.vhd \
+../../../../Minimig_ECS/impl1/source/TG68K_Pack.vhd \
+../../../../Minimig_ECS/impl1/source/TG68KdotC_Kernel.vhd \
+../../../../Minimig_ECS/impl1/source/HDMI/aux_ecc1.vhd \
+../../../../Minimig_ECS/impl1/source/HDMI/aux_ecc2.vhd \
+../../../../Minimig_ECS/impl1/source/HDMI/aux_encoder.vhd \
+../../../../source_emard/HDMI/dvid.vhd \
+../../../../Minimig_ECS/impl1/source/HDMI/dvienc_defs.vhd \
+../../../../Minimig_ECS/impl1/source/HDMI/edvi_ucode.vhd \
+../../../../Minimig_ECS/impl1/source/HDMI/infoframe_rom_800x600_60hz_40M_48k.vhd \
+../../../../Minimig_ECS/impl1/source/HDMI/TMDS_encoder.vhd \
+../../../../source_emard/poweronreset.vhd \
+../../../../osd_bootstrap.vhd \
+../../../../jbboot.vhd \
+../../../../rtl_emard/generic/bram_true2p_1clk.vhd \
+../../../../rtl_emard/vga/vga.vhd \
+../../../../rtl_emard/vga/hdmi/vga2dvid.vhd \
+../../../../rtl_emard/vga/hdmi/tmds_encoder.vhd \
+../../../../rtl_emard/vga/lattice/ecp5u/ddr_out_emard.vhd \
+../../../../rtl_emard/vga/adv/i2c_sender.vhd \
+../../../../rtl_emard/spdif/spdif_tx.vhd \
+../../../../rtl_emard/osd/char_rom.vhd \
+../../../../rtl_emard/generic/bram_true2p_1clk.vhd \
+../../../../Minimig_ECS/RTL/sdram_cache.vhd \
+
+
+#../../../../rtl_emard/usb/usbhid/usbhid_report_decoder_saitek_joystick.vhd \
+#../../../../rtl_emard/usb/usbhid/report_decoded_pack_generic.vhd \
+#../../../../rtl_emard/usb/usbhost/usbh_setup_pack.vhd \
+#../../../../rtl_emard/usb/usbhost/usbh_host_hid.vhd \
+#../../../../rtl_emard/usb/usbhost/usbh_sie_vhdl.vhd \
+#../../../../rtl_emard/usb/usb11_phy_vhdl/usb_phy.vhd \
+#../../../../rtl_emard/usb/usb11_phy_vhdl/usb_rx_phy_48MHz.vhd \
+#../../../../rtl_emard/usb/usb11_phy_vhdl/usb_tx_phy.vhd \
+
+
+# sdram controllers
+# ../../../../rtl_emard/sdram/sdram.vhd \
+# ../../../../Minimig_ECS/RTL/sdram_cache.vhd \
+
+VERILOG_FILES = \
+../../../../rtl_emard/lattice/ulx3s/clocks/$(CLK0_NAME).v \
+../../../../rtl_emard/lattice/ulx3s/clocks/$(CLK1_NAME).v \
+../../../../rtl_emard/lattice/ulx3s/clocks/$(CLK2_NAME).v \
+../../../../rtl_emard/lattice/ulx3s/clocks/$(CLK3_NAME).v \
+../../../../Minimig_ECS/impl1/source/Agnus.v \
+../../../../Minimig_ECS/impl1/source/Amber.v \
+../../../../Minimig_ECS/impl1/source/Audio.v \
+../../../../Minimig_ECS/impl1/source/Beamcounter.v \
+../../../../Minimig_ECS/impl1/source/Bitplanes.v \
+../../../../Minimig_ECS/impl1/source/Blitter.v \
+../../../../Minimig_ECS/impl1/source/CIA8520.v \
+../../../../Minimig_ECS/impl1/source/Clock.v \
+../../../../Minimig_ECS/impl1/source/Copper.v \
+../../../../Minimig_ECS/impl1/source/Denise.v \
+../../../../Minimig_ECS/impl1/source/Floppy.v \
+../../../../Minimig_ECS/impl1/source/Gary.v \
+../../../../Minimig_ECS/impl1/source/Gayle.v \
+../../../../source_emard/Minimig1.v \
+../../../../Minimig_ECS/impl1/source/Paula.v \
+../../../../source_emard/PS2Keyboard.v \
+../../../../source_emard/twowaycache.v \
+../../../../Minimig_ECS/impl1/source/Sprites.v \
+../../../../source_emard/Userio.v \
+../../../../rtl_emard/usb/usbhost/usbh_sie.v \
+../../../../rtl_emard/usb/usbhost/usbh_crc5.v \
+../../../../rtl_emard/usb/usbhost/usbh_crc16.v \
