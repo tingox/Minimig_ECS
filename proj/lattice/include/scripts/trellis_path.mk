aa4daf5e5419d400c786f8d37e0de8a213d33e13
Torfinn Ingolfsen <tingox@gmail.com>
2020-11-20 11:36:29 +0100

    fix paths to tools

--- proj/lattice/include/scripts/trellis_path.mk
+++ proj/lattice/include/scripts/trellis_path.mk
@@ -1,9 +1,9 @@
 # ******* tools installation paths *******
 # https://github.com/ldoolitt/vhd2vl
-VHDL2VL ?= /mt/scratch/tmp/openfpga/vhd2vl/src/vhd2vl
+VHDL2VL ?= vhd2vl
 # https://github.com/YosysHQ/yosys
-YOSYS ?= /mt/scratch/tmp/openfpga/yosys/yosys
+YOSYS ?= yosys
 # https://github.com/YosysHQ/nextpnr
-NEXTPNR-ECP5 ?= /mt/scratch/tmp/openfpga/nextpnr/nextpnr-ecp5
+NEXTPNR-ECP5 ?= nextpnr-ecp5
 # https://github.com/SymbiFlow/prjtrellis
-TRELLIS ?= /mt/scratch/tmp/openfpga/prjtrellis
+#TRELLIS ?= /mt/scratch/tmp/openfpga/prjtrellis
