11d5f98d0dacf5d9515f3d9b045374183cb8cc29
Torfinn Ingolfsen <tingox@gmail.com>
2020-11-20 11:50:48 +0100

    fix up ecppll lines

--- proj/lattice/include/scripts/trellis_main.mk
+++ proj/lattice/include/scripts/trellis_main.mk
@@ -130,19 +130,19 @@ $(BOARD)_$(FPGA_SIZE)f_$(PROJECT).bit: $(BOARD)_$(FPGA_SIZE)f_$(PROJECT).config
 	LANG=C $(ECPPACK) $(IDCODE_CHIPID) --compress --input $< --bit $@
 
 $(CLK0_FILE_NAME):
-	LANG=C LD_LIBRARY_PATH=$(LIBTRELLIS) $(ECPPLL) $(CLK0_OPTIONS) --file $@
+	LANG=C $(ECPPLL) $(CLK0_OPTIONS) --file $@
 	sed -e "s/module pll(/module $(CLK0_NAME)(/g" -i $@
 
 $(CLK1_FILE_NAME):
-	LANG=C LD_LIBRARY_PATH=$(LIBTRELLIS) $(ECPPLL) $(CLK1_OPTIONS) --file $@
+	LANG=C $(ECPPLL) $(CLK1_OPTIONS) --file $@
 	sed -e "s/module pll(/module $(CLK1_NAME)(/g" -i $@
 
 $(CLK2_FILE_NAME):
-	LANG=C LD_LIBRARY_PATH=$(LIBTRELLIS) $(ECPPLL) $(CLK2_OPTIONS) --file $@
+	LANG=C $(ECPPLL) $(CLK2_OPTIONS) --file $@
 	sed -e "s/module pll(/module $(CLK2_NAME)(/g" -i $@
 
 $(CLK3_FILE_NAME):
-	LANG=C LD_LIBRARY_PATH=$(LIBTRELLIS) $(ECPPLL) $(CLK3_OPTIONS) --file $@
+	LANG=C $(ECPPLL) $(CLK3_OPTIONS) --file $@
 	sed -e "s/module pll(/module $(CLK3_NAME)(/g" -i $@
 
 # generate XCF programming file for DDTCMD
