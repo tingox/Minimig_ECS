58d009478a7ecc55d1173fc0210873a8a10f86d3
Torfinn Ingolfsen <tingox@gmail.com>
2020-11-20 11:52:27 +0100

    sed on FreeBSD doesn't allow -i when doing std input

--- proj/lattice/include/scripts/trellis_main.mk
+++ proj/lattice/include/scripts/trellis_main.mk
@@ -131,19 +131,19 @@ $(BOARD)_$(FPGA_SIZE)f_$(PROJECT).bit: $(BOARD)_$(FPGA_SIZE)f_$(PROJECT).config
 
 $(CLK0_FILE_NAME):
 	LANG=C $(ECPPLL) $(CLK0_OPTIONS) --file $@
-	sed -e "s/module pll(/module $(CLK0_NAME)(/g" -i $@
+	sed -e "s/module pll(/module $(CLK0_NAME)(/g" $@
 
 $(CLK1_FILE_NAME):
 	LANG=C $(ECPPLL) $(CLK1_OPTIONS) --file $@
-	sed -e "s/module pll(/module $(CLK1_NAME)(/g" -i $@
+	sed -e "s/module pll(/module $(CLK1_NAME)(/g" $@
 
 $(CLK2_FILE_NAME):
 	LANG=C $(ECPPLL) $(CLK2_OPTIONS) --file $@
-	sed -e "s/module pll(/module $(CLK2_NAME)(/g" -i $@
+	sed -e "s/module pll(/module $(CLK2_NAME)(/g" $@
 
 $(CLK3_FILE_NAME):
 	LANG=C $(ECPPLL) $(CLK3_OPTIONS) --file $@
-	sed -e "s/module pll(/module $(CLK3_NAME)(/g" -i $@
+	sed -e "s/module pll(/module $(CLK3_NAME)(/g" $@
 
 # generate XCF programming file for DDTCMD
 $(BOARD)_$(FPGA_SIZE)f.xcf: $(BOARD)_$(FPGA_SIZE)f_$(PROJECT).bit $(SCRIPTS)/$(BOARD)_sram.xcf $(SCRIPTS)/xcf.xsl $(DTD_FILE)
